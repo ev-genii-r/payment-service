704bb2e0cd2e639aa68f63f0a209c44a24e90a27
ev-genii-r <ev_genii_r@mail.ru>
2025-12-11 16:51:06 +0300

    task2_V2

--- src/main/java/com/innowise/rudkovskii/service/PaymentService.java
+++ src/main/java/com/innowise/rudkovskii/service/PaymentService.java
@@ -1,7 +1,43 @@
 package com.innowise.rudkovskii.service;
 
+import com.innowise.rudkovskii.entity.Payment;
+import com.innowise.rudkovskii.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
 public class PaymentService {
 
-    
+    PaymentRepository repository;
+
+    @Autowired
+    public PaymentService(PaymentRepository repository){
+        this.repository = repository;
+    }
+
+    @Transactional
+    public Payment createPayment(Payment payment){
+        return repository.save(payment);
+    }
+
+    public List<Payment> getPaymentsByOrderId(Long orderId){
+        return repository.findPaymentsByOrderId(orderId);
+    }
+
+    public List<Payment> getPaymentsByUserId(Long userId){
+        return repository.findPaymentsByUserId(userId);
+    }
+
+    public List<Payment> getPaymentsByStatus(String status){
+        return repository.findPaymentsByStatus(status);
+    }
+
+    public double getTotalSumForPeriod(LocalDateTime startDate, LocalDateTime endDate){
+        return repository.getTotalSumByDatePeriod(startDate, endDate);
+    }
 
 }
